0cd5762825f8899bee2d77513dfc7f1644c3b1a8
Mark Cartwright <sirloper@gmail.com>
2014-12-01 08:11:23 -0500

    Fix for hostname and Serial number
    Merging fixes for hostname as well as a handler for Rundeck issues where the serial number is a pure int but cast as a string.

--- puppetdb-rundeck.rb
+++ puppetdb-rundeck.rb
@@ -7,9 +7,9 @@ require 'yaml'
 require 'sinatra'
 
 # Base URL of the PuppetDB database.  Do not include a trailing slash!
-host_uri = 'http://<%= @puppetdb_host %>'
+host_uri = 'http://localhost'
 # Port number for the PuppetDB REST interface -- default is 8080 for clear, 8081 for SSL.
-port = '<%= @puppetdb_port %>'
+port = '8080'
 
 puppetdb_resource_query = {'query'=>'["=", "type", "Class"],]'}
 
@@ -18,46 +18,48 @@ before do
 end
 
 get '/' do
-  uri = URI.parse( "#{host_uri}:#{port}/v3/resources" )
-  http = Net::HTTP.new(uri.host, uri.port) 
-  request = Net::HTTP::Get.new(uri.path) 
-  request.set_form_data( puppetdb_resource_query )
-  request = Net::HTTP::Get.new( uri.path+ '?' + request.body ) 
-  request.add_field("Accept", "application/json")
-  response = http.request(request)
-  puppetdb_data = JSON.parse(response.body)
+	uri = URI.parse( "#{host_uri}:#{port}/v3/resources" )
+	http = Net::HTTP.new(uri.host, uri.port) 
+	request = Net::HTTP::Get.new(uri.path) 
+	request.set_form_data( puppetdb_resource_query )
+	request = Net::HTTP::Get.new( uri.path+ '?' + request.body ) 
+	request.add_field("Accept", "application/json")
+	response = http.request(request)
+	puppetdb_data = JSON.parse(response.body)
 
-  rundeck_resources = Hash.new
-  puppetdb_data.each{|d|
-    host     = d['certname']
-    title    = d['title']
-    rundeck_resources[host] = Hash.new if not rundeck_resources.key?(host)
-    rundeck_resources[host]['tags'] = Array.new if not rundeck_resources[host].key?('tags')
-    rundeck_resources[host]['tags'] << title
-  }
+	rundeck_resources = Hash.new
+	puppetdb_data.each{|d|
+	host     = d['certname']
+	title    = d['title']
+	rundeck_resources[host] = Hash.new if not rundeck_resources.key?(host)
+	rundeck_resources[host]['tags'] = Array.new if not rundeck_resources[host].key?('tags')
+	rundeck_resources[host]['tags'] << title
+	}
+	
+	rundeck_resources.keys.sort.each { |k|
+	rundeck_resources[k]['tags'].uniq!
+	rundeck_resources[k]['tags'] =  rundeck_resources[k]['tags'].join(",")
+	rundeck_resources[k]['hostname'] = k
+	}
+	
+	uri = URI.parse( "#{host_uri}:#{port}/v3/facts" )
+	http = Net::HTTP.new(uri.host, uri.port) 
+	request = Net::HTTP::Get.new(uri.path) 
+	request = Net::HTTP::Get.new(uri.path)
+	request.add_field("Accept", "application/json")
+	response = http.request(request)
+	puppetdb_data = JSON.parse(response.body)
+		
+	puppetdb_data.each{|d|
+	host     = d['certname']
+	name     = d['name'] if d['name'] != "hostname"
+  value    = d['value'] if d['name'] != "hostname"
+  if ( name == 'serialnumber' )
+    rundeck_resources[host][name] = 'Serial Number ' + value
+  else
+		rundeck_resources[host][name] = value
+	end
+	}
 
-  rundeck_resources.keys.sort.each { |k|
-    rundeck_resources[k]['tags'].uniq!
-    rundeck_resources[k]['tags'] =  rundeck_resources[k]['tags'].join(",")
-    rundeck_resources[k]['hostname'] = k
-  }
-
-  uri = URI.parse( "#{host_uri}:#{port}/v3/facts" )
-  http = Net::HTTP.new(uri.host, uri.port) 
-  request = Net::HTTP::Get.new(uri.path) 
-  request = Net::HTTP::Get.new(uri.path)
-  request.add_field("Accept", "application/json")
-  response = http.request(request)
-  puppetdb_data = JSON.parse(response.body)
-
-  puppetdb_data.each{|d|
-    next if d['name'] == "hostname"
-    host     = d['certname']
-    name     = d['name']
-    value    = d['value']
-    rundeck_resources[host] ||= {}
-    rundeck_resources[host][name] = value
-  }
-
-  rundeck_resources.to_yaml
+	rundeck_resources.to_yaml
 end
